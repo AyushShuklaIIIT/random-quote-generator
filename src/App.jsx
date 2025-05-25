4175cd0d483c7a53291165338d2289fed6e51bf6
Ayush Shukla <shuklaayush628@gmail.com>
2025-05-25 18:14:12 +0530

    Changed the source API

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+// import Input from "./components/Input";
+// import { searchContext } from "./context/contexts";
 
 function App() {
   const [color, setColor] = useState({ r: 50, g: 50, b: 50 });
   const [quotes, setQuotes] = useState([]);
   const [currQuote, setcurrQuote] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [link, setLink] = useState(null);
+  const [xlink, setxLink] = useState(null);
   const [wpLink, setwpLink] = useState(null);
   const [error, setError] = useState(null);
 
@@ -24,33 +26,39 @@ function App() {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     setcurrQuote(randomQuote);
     setColor(getRandomColor());
-    setLink(
-      `https://x.com/intent/tweet?text="${randomQuote.content}"%20${randomQuote.author}&hashtags=quotes`
+    setxLink(
+      `https://x.com/intent/tweet?text="${randomQuote.quote}"%20${randomQuote.author}&hashtags=quotes`
     );
     setwpLink(
-      `https://wa.me/?text="${randomQuote.content}"%20${randomQuote.author}`
+      `https://wa.me/?text="${randomQuote.quote}"%20${randomQuote.author}`
     );
   };
 
   const getQuotes = async () => {
     try {
       const response = await fetch(
-        "https://api.quotable.io/quotes/random?limit=20"
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
       );
       if (!response.ok) {
         throw new Error(`Server error: ${response.status}`);
       }
-      const result = await response.json();
+      let result = await response.json();
+      result = result.quotes;
       setQuotes(result);
       console.log(result);
       const randomQuote = result[Math.floor(Math.random() * result.length)];
       setcurrQuote(randomQuote);
       setColor(getRandomColor());
-      setLink(
-        `https://x.com/intent/tweet?text="${randomQuote.content}"%20${randomQuote.author}&hashtags=quotes`
+      setxLink(
+        `https://x.com/intent/tweet?text="${randomQuote.quote}"%20${randomQuote.author}&hashtags=quotes`
       );
       setwpLink(
-        `https://wa.me/?text="${randomQuote.content}"%20${randomQuote.author}`
+        `https://wa.me/?text="${randomQuote.quote}"%20${randomQuote.author}`
       );
     } catch (error) {
       console.error("Failed to fetch quotes: ", error);
@@ -71,6 +79,9 @@ function App() {
         duration-1000"
         style={{ backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})` }}
       >
+        {/* <searchContext.Provider value={color}>
+          <Input></Input>
+        </searchContext.Provider> */}
         <div
           id="quote-box"
           className="w-full max-w-[550px] bg-white rounded-sm p-9 transition-all duration-1000 shadow-md m-3"
@@ -78,7 +89,7 @@ function App() {
           <AnimatePresence mode="wait">
             {!loading && currQuote && (
               <motion.div
-                key={currQuote.content}
+                key={currQuote.quote}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
@@ -91,14 +102,18 @@ function App() {
                   className="text-3xl transition duration-1000 text-center justify-center"
                 >
                   <FontAwesomeIcon icon={faQuoteLeft} className="mr-2" />
-                  <span className="playfair-display-content">{currQuote.content}</span>
+                  <span className="playfair-display-content">
+                    {currQuote.quote}
+                  </span>
                 </div>
                 <div
                   id="author"
                   style={{ color: `rgb(${color.r}, ${color.g}, ${color.b})` }}
                   className="transition duration-1000 text-right text-lg mt-4"
                 >
-                  <span className="playfair-display-content">-- {currQuote.author}</span>
+                  <span className="playfair-display-content">
+                    -- {currQuote.author}
+                  </span>
                 </div>
               </motion.div>
             )}
@@ -110,7 +125,7 @@ function App() {
           </AnimatePresence>
           <div id="button-div" className="flex justify-between mt-8">
             <div id="message-buttons" className="flex gap-2">
-              <a href={link || ""} target="_blank" rel="noopener noreferrer">
+              <a href={xlink || ""} target="_blank" rel="noopener noreferrer">
                 <button
                   id="tweet-quote"
                   style={{
