6a331126c73b2d47278da8e594d5a1cc2b01beaf
Ayush Shukla <shuklaayush628@gmail.com>
2025-05-29 19:20:44 +0530

    Changed again

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ isOpen, onClose, color, children }) => {
       className="fixed inset-0 bg-opacity-40 flex items-center justify-center z-50"
       style={{ backgroundColor: `rgba(${color.r}, ${color.g}, ${color.b})` }}
     >
-      <div className="bg-white rounded-xl shadow-lg p-6 max-w-[550px] w-full relative transition-all duration-1000 outline outline-black/5 m-3 max-h-screen overflow-auto scale-65 md:scale-100">
+      <div className="bg-white rounded-xl shadow-lg p-6 max-w-[550px] w-full relative transition-all duration-1000 outline outline-black/5 m-3 max-h-screen overflow-auto">
         <button
           onClick={onClose}
           className="absolute top-2 right-3 text-gray-600 hover:text-black font-bold text-3xl cursor-pointer"
